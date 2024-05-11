72839236ec92b604297ff7718a0ca47c91d3968e
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-05-11 19:39:20 +0300

    add `how-react-works`, `usepopcorn` projects

---
+++ course-projects/usepopcorn/src/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="darkslategray" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <>
+      <StarRating
+        maxRating={5}
+        messages={["🤮", "🥴", "😐", "😊", "🤩"]}
+        defaultRating="3"
+      />
+      <Test />
+    </>
+  </React.StrictMode>
+);
