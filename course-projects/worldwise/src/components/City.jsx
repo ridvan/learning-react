ad53091725085fd769e8f661d260de34ce047c91
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-07-15 13:38:05 +0300

    fix(worldwise): useCallback to prevent infinite getCity calls

--- course-projects/worldwise/src/components/City.jsx
+++ course-projects/worldwise/src/components/City.jsx
@@ -1,6 +1,6 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
@@ -21,7 +21,7 @@ function City() {
     function () {
       getCity(cityId);
     },
-    [cityId]
+    [cityId, getCity]
   );
 
   if (isLoading) {
--- course-projects/worldwise/src/contexts/CitiesContext.jsx
+++ course-projects/worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useEffect, useContext, useReducer } from "react";
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from "react";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -64,21 +70,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: "loading" });
-    try {
-      const request = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await request.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "An error occurred while fetching city data",
-      });
-    }
-  }
+      dispatch({ type: "loading" });
+      try {
+        const request = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await request.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "An error occurred while fetching city data",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function addCity(newCity) {
     dispatch({ type: "loading" });
