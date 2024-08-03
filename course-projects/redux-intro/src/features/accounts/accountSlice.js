ac6a74e0400476e06ba364147ce2a4001c9dac9c
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-08-03 23:46:17 +0300

    refactor(redux-intro): create accountSlice with redux toolkit

--- course-projects/redux-intro/src/features/accounts/accountSlice.js
+++ course-projects/redux-intro/src/features/accounts/accountSlice.js
@@ -1,10 +1,49 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.loanPurpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export default accountSlice.reducer;
+
+/*
+// THE OLD WAY OF CREATING SLICE
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
@@ -71,3 +110,4 @@ export function requestLoan(amount, loanPurpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+*/
