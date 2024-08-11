cfe6281b663514749cca905ecdb02aa15436a603
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-08-11 14:34:09 +0300

    feat(fast-react-pizza): fetch menu with useLoaderData in React router
    Render as you fetch strategy

--- course-projects/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ course-projects/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,26 @@
+import PropTypes from "prop-types";
+import { formatCurrency } from "../../utils/helpers";
+
+MenuItem.propTypes = {
+  pizza: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    unitPrice: PropTypes.number.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    soldOut: PropTypes.bool.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
