3acdfc5be49db3bf8b3acdf85d55a5e51dc23f2c
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-10-26 23:38:27 +0300

    feat(fast-react-pizza): update tailwind config, reuse styles

--- course-projects/fast-react-pizza/src/ui/Button.jsx
+++ course-projects/fast-react-pizza/src/ui/Button.jsx
@@ -1,16 +1,25 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   disabled: PropTypes.bool.isRequired,
+  to: PropTypes.string,
 };
 
-function Button({ children, disabled }) {
+function Button({ children, disabled, to }) {
+  const className =
+    "rounded-xl bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+  if (to) {
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      className="rounded-xl bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-      disabled={disabled}
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
--- course-projects/fast-react-pizza/tailwind.config.js
+++ course-projects/fast-react-pizza/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    // colors: {
+    //   river: "#2F4F4F",
+    // },
   },
   plugins: [],
 };
