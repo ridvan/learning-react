b9d115c702dacae88b8d0b033961ec4978a86a04
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-10-27 17:21:59 +0300

    feat(fast-react-pizza): update Cart component style

--- course-projects/fast-react-pizza/src/features/cart/CartItem.jsx
+++ course-projects/fast-react-pizza/src/features/cart/CartItem.jsx
@@ -1,13 +1,27 @@
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
+import PropTypes from "prop-types";
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    pizzaId: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li id={`pizza-${pizzaId}`} className="py-3 sm:flex sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
