1a4f430af86997a4b37c07890e5db90fd6aae4bc
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-10-28 00:57:08 +0300

    feat(fast-react-pizza): create userSlice, read/update User state

--- course-projects/fast-react-pizza/src/features/cart/CartOverview.jsx
+++ course-projects/fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function CartOverview() {
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
@@ -5,7 +7,7 @@ function CartOverview() {
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <a href="/cart">Open cart &rarr;</a>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
