2583caa66d1ac131e498986341bf5f0f55809957
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-11-03 19:10:20 +0300

    feat(two/db): upload cabin image to supabase bucket

--- course-projects/the-wild-oasis/src/services/apiCabins.js
+++ course-projects/the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,11 +12,33 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random().toFixed(5)}-${
+    newCabin.image.name
+  }`.replaceAll(/[^a-zA-Z0-9.-]/g, "-");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert({ ...newCabin, image: imagePath });
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
+  }
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded so cabin was not created"
+    );
   }
 
   return data;
--- course-projects/the-wild-oasis/src/services/supabase.js
+++ course-projects/the-wild-oasis/src/services/supabase.js
@@ -4,3 +4,4 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+export { supabaseUrl };
