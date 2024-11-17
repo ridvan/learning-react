d3eb8e6859b797509e2dfa2f99822163c3a9e6e8
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-11-17 19:12:24 +0300

    feat(two/ui): create add cabin modal, use `createPortal`

--- course-projects/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ course-projects/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
   const isWorking = isCreating || isEditing;
@@ -29,12 +29,22 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     if (isEditSession)
       editCabin(
         { newCabinData: { ...data, image }, id: editId },
-        { onSuccess: () => reset() }
+        {
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
+        }
       );
     else
       createCabin(
         { ...data, image: data.image[0] },
-        { onSuccess: () => reset() }
+        {
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
+        }
       );
   }
 
@@ -42,7 +52,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     // console.log(errors);
   }
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -126,7 +139,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
@@ -146,6 +163,7 @@ CreateCabinForm.propTypes = {
     description: PropTypes.string,
     image: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   }),
+  onCloseModal: PropTypes.func,
 };
 
 export default CreateCabinForm;
