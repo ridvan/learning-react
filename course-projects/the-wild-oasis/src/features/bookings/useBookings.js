e3ecbb8f5ee18e95f31f3651b21c023d5b7268d9
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-12-21 16:03:51 +0300

    feat(two/ui): add sort by feature for bookings

--- course-projects/the-wild-oasis/src/features/bookings/useBookings.js
+++ course-projects/the-wild-oasis/src/features/bookings/useBookings.js
@@ -14,13 +14,18 @@ export function useBookings() {
 
   console.log(filter);
 
+  // Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
