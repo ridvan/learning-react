f228059c4744c85a31144689b0e55d8820771112
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-12-23 20:21:34 +0300

    feat(two/ui): display activities of current day

--- course-projects/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ course-projects/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,7 +19,6 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
   const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
   const {
-    stays,
     confirmedStays,
     isLoading: isLoadingStays,
     numDays,
@@ -36,7 +36,7 @@ function DashboardLayout() {
         cabinCount={cabins?.length}
         numDays={numDays}
       />
-      <div>Today&apos;s activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
