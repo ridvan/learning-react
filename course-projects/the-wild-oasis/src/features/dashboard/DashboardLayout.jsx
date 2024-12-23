fadf5816ee27ffb0b42edeea18704c053d25ee01
Rıdvan <63660173+ridvan@users.noreply.github.com>
2024-12-23 18:14:18 +0300

    feat(two/ui): compute recent bookings and stays

--- course-projects/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ course-projects/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable no-unused-vars */
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +10,19 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoadingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoadingStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
