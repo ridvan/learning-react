dcfafbb766c899d7ab41f544c3bb224463579b58
Rıdvan <63660173+ridvan@users.noreply.github.com>
2025-01-04 22:07:52 +0300

    feat(two-web): add navigation and header, update metadata

--- course-projects/the-wild-oasis-website/app/cabins/page.js
+++ course-projects/the-wild-oasis-website/app/cabins/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Cabins",
+};
+
 export default function Page() {
   return <h1>Cabins</h1>;
 }
--- course-projects/the-wild-oasis-website/app/layout.js
+++ course-projects/the-wild-oasis-website/app/layout.js
@@ -1,21 +1,34 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "The Wild Oasis",
+  },
+  description:
+    "Luxury and comfort in the heart of nature, The Wild Oasis offers cabins for rent in the beautiful wilderness of Sapanca.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-gray-50 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} bg-primary-950 text-gray-50 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-500">{children}</main>
+        </div>
       </body>
     </html>
   );
