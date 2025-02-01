0e5bd5bbb974ea2069c1ba99202d88da3cfd3ae3
Rıdvan <63660173+ridvan@users.noreply.github.com>
2025-02-02 00:18:44 +0300

    feat(two-web): add reservation context and reminder component

--- course-projects/the-wild-oasis-website/app/layout.js
+++ course-projects/the-wild-oasis-website/app/layout.js
@@ -1,9 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
-import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
-import Header from "./_components/Header";
+import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
+import { ReservationProvider } from "@/app/_components/ReservationContext";
 
 const josefin = Josefin_Sans({
   display: "swap",
@@ -27,7 +25,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
       </body>
     </html>
