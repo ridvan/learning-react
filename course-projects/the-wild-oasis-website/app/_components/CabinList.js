0cae8a622202f7867710abd4b110fed077847f3b
Rıdvan <63660173+ridvan@users.noreply.github.com>
2025-02-01 18:28:24 +0300

    feat(two-web): add cabin filtering functionality
    Using `useSearchParams `, `useRouter `, `usePathname` hooks from next/navigation, we shared the state between client and server via URL.

--- course-projects/the-wild-oasis-website/app/_components/CabinList.js
+++ course-projects/the-wild-oasis-website/app/_components/CabinList.js
@@ -1,14 +1,26 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  const filterFunctions = {
+    all: () => cabins,
+    small: () => cabins.filter((cabin) => cabin.maxCapacity <= 3),
+    medium: () =>
+      cabins.filter(
+        (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+      ),
+    large: () => cabins.filter((cabin) => cabin.maxCapacity >= 8),
+  };
+
+  const displayedCabins = filterFunctions[filter]();
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
