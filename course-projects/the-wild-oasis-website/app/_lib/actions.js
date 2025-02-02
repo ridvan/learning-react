66a8567bbd632d270cfdc4ad883322179a91a31b
Rıdvan <63660173+ridvan@users.noreply.github.com>
2025-02-02 23:02:17 +0300

    feat(two-web): add guest profile update functionality
    - Used `revalidatePath` to revalidate cached data and fetch the updated data on the client
    - Used `useFormStatus` hook to get the pending status of the form while the server action is running.

--- course-projects/the-wild-oasis-website/app/_lib/actions.js
+++ course-projects/the-wild-oasis-website/app/_lib/actions.js
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -8,4 +10,33 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in to update your profile");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  const idPattern = /^[a-zA-Z0-9]{6,12}$/;
+  if (!idPattern.test(nationalID)) {
+    throw new Error(
+      "National ID must be alphanumeric and between 6 and 12 characters long"
+    );
+  }
+
+  const updateData = { nationality, nationalID, countryFlag };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  // Revalidate cached data and fetch the updated data on the client
+  revalidatePath("/account/profile");
+
+  return data;
 }
